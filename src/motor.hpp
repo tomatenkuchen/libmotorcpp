7c1a694db6e40251b77cbe022fc0b865babaf114
tomatenkuchen <t.w92@gmx.de>
2025-11-22 15:37:20 +0100

    reintroduced logic. compiles and links

--- src/motor.hpp
+++ src/motor.hpp
@@ -33,24 +33,46 @@ struct Motor
         ccw,
     };
 
-    using radspeed = decltype(units::rad / si::second);
-    using radacceleration = decltype(units::rad / (si::second * si::second));
+    // define radial speed quantity type
+    inline static constexpr auto radspeed = units::rad / si::second;
+    // define radial acceleration quantity type
+    inline static constexpr auto radacceleration = units::rad / (si::second * si::second);
 
     struct State
     {
         quantity<units::rad, float> position;
-        // quantity<radspeed, float> speed;
-        // quantity<radacceleration, float> acceleration;
+        quantity<radspeed, float> speed;
+        quantity<radacceleration, float> acceleration;
     };
 
     State state;
     quantity<si::second, float> hall_isr_timestamp;
     int8_t last_sector;
 
-    void interrupt_handler(quantity<si::second, float> system_time, int8_t new_sector);
+    /**
+     * @brief interrupt handler to be called when hall sensor generate new input
+     * 
+     * @param system_time system time of hall sensor interrupt
+     * @param new_sector  new sector hall sensor determined rotor to be in
+     */
+    void hall_interrupt_handler(quantity<si::second, float> system_time, int8_t new_sector);
 
+    /**
+     * @brief get the current motor state
+     * 
+     * @param system_time motor rotation angle is approximated based on last known speed and acceleration. system time is needed for extrapolation
+     * @return State current motor state
+     */
     State get_motor_state(quantity<si::second, float> system_time);
 
+    private:
+
+    /**
+     * @brief Get the rotation direction object 
+     * 
+     * @param new_sector  new sector hall sensor determined rotor to be in
+     * @return Rotation   current rotation direction
+     */
     Rotation get_rotation_direction(int8_t new_sector);
 };
 
